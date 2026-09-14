7e891da19a2d1ab7c6ea76486ed40ea23e852601
HimanshuHarinkhede <167709128+HimanshuHarinkhede@users.noreply.github.com>
2026-09-14 18:29:00 +0530

    Time: 10 ms (17.78%) | Memory: 47.1 MB (7.67%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i =ss.length -1; i>=0; i--){
             sss += ss[i] + " ";
         }
-        sss = sss.trim(); //triming again
+        sss = sss.trim(); //triming again for last space
        return sss;
     }
 }
